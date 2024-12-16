6c7df7853712c25243bfec5d236f34bdf20d7e0b
SURUZO <sooraj.s1289@gmail.com>
2024-12-16 20:37:41 +0530

    admin login

--- attendance-backend/config/db.js
+++ attendance-backend/config/db.js
@@ -1,9 +1,8 @@
-// config/db.js
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log('MongoDB Connected');
   } catch (err) {
     console.error('Database connection error:', err.message);
--- attendance-backend/routes/userRoutes.js
+++ attendance-backend/routes/userRoutes.js
@@ -1,4 +1,3 @@
-// routes/userRoutes.js
 const express = require('express');
 const { registerUser, loginUser, getAllUsers } = require('../controllers/userController');
 const router = express.Router();
@@ -8,4 +7,3 @@ router.post('/login', loginUser);
 router.get('/all', getAllUsers); // New endpoint to list all users with IDs and usernames
 
 module.exports = router;
-
